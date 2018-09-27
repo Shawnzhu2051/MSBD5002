ebbc621075b7ccbe3d02a02def9c2ab9362e197d
Shawnzhu2051 <shawnzhu2051@gmail.com>
2018-09-27 19:32:32 +0800

    2018-9-27

--- Ass1/Task2.py
+++ Ass1/Task2.py
@@ -3,16 +3,16 @@ import csv
 MINSUP = 300
 
 class DictValue(object):
-    def __init__(self):
-        self.frequency = 0
-        self.link2tree = None
+    def __init__(self, frequency):
+        self.frequency = frequency
+        self.node_link = None
 
 
 class TreeNode(object):
     def __init__(self, key, frequency, parent_node):
         self.key = key
         self.frequency = frequency
-        self.nodeLink = None
+        self.node_link = None
         self.parent = parent_node
         self.children = {}
 
@@ -45,11 +45,12 @@ def compute_item_frequency(data):
             if item_name in head_table:
                 head_table[item_name].frequency += 1
             else:
-                new_dic_value = DictValue()
+                new_dic_value = DictValue(0)
                 head_table[item_name] = new_dic_value
     for item_name in list(head_table):
-        if head_table[item_name].frequency < 300:
+        if head_table[item_name].frequency < MINSUP:
             head_table.pop(item_name)
+    head_table = dict(sorted(head_table.items(), key=lambda x: x[1].frequency, reverse=True))
     return head_table
 
 
@@ -57,87 +58,128 @@ def compute_frequent_item(data, head_table):
     item_frequency_list = list(head_table)
     ret_data = []
     for row in data:
+        new_row = []
         for item_name in row:
-            if item_name not in head_table:
-                row.remove(item_name)
-        row_sorted = [row for (row, item_frequency_list) in sorted(zip(row, item_frequency_list))]
-        if len(row_sorted) != 0:
+            if item_name in head_table:
+                new_row.append(item_name)
+        if len(new_row) != 0:
+            row_sorted = [new_row for (new_row, item_frequency_list) in sorted(zip(new_row, item_frequency_list))]
             ret_data.append(row_sorted)
     return ret_data
 
-'''
-def updateHeader(nodeToTest, targetNode):
-    while nodeToTest.nodeLink != None:
-        nodeToTest = nodeToTest.nodeLink
-    nodeToTest.nodeLink = targetNode
-    
-def updateFPtree(items, inTree, headerTable, count):
-    if items[0] in inTree.children:
-        # 判断items的第一个结点是否已作为子结点
-        inTree.children[items[0]].inc(count)
-    else:
-        # 创建新的分支
-        inTree.children[items[0]] = TreeNode(items[0], count, inTree)
-        # 更新相应频繁项集的链表，往后添加
-        if headerTable[items[0]][1] == None:
-            headerTable[items[0]][1] = inTree.children[items[0]]
-        else:
-            updateHeader(headerTable[items[0]][1], inTree.children[items[0]])
-    # 递归
-    if len(items) > 1:
-        updateFPtree(items[1::], inTree.children[items[0]], headerTable, count)
-'''
-def update_Header(nodeToTest, targetNode):
-    pass
+
+def update_header(start_header_name, targetNode):
+    while start_header_name.node_link is not None:
+        start_header_name = start_header_name.node_link
+    start_header_name.node_link = targetNode
+
 
 def update_FPtree(frequent_item, root_node, head_table):
     if frequent_item[0] in root_node.children:
         root_node.children[frequent_item[0]].increase()
     else:
-        pass
+        root_node.children[frequent_item[0]] = TreeNode(frequent_item[0], 1, root_node)
+        if head_table[frequent_item[0]].node_link is None:
+            head_table[frequent_item[0]].node_link = root_node.children[frequent_item[0]]
+        else:
+            update_header(head_table[frequent_item[0]], root_node.children[frequent_item[0]])
     if len(frequent_item) > 1:
         update_FPtree(frequent_item[1::], root_node.children[frequent_item[0]], head_table)
 
-'''
-def createFPtree(dataSet, minSup=1):
-    headerTable = {}
-    for trans in dataSet:
-        for item in trans:
-            headerTable[item] = headerTable.get(item, 0) + dataSet[trans]
-    for k in headerTable.keys():
-        if headerTable[k] < minSup:
-            del(headerTable[k]) # 删除不满足最小支持度的元素
-    freqItemSet = set(headerTable.keys()) # 满足最小支持度的频繁项集
-    if len(freqItemSet) == 0:
-        return None, None
-    for k in headerTable:
-        headerTable[k] = [headerTable[k], None] # element: [count, node]
 
-    retTree = TreeNode('Null Set', 1, None)
-    for tranSet, count in dataSet.items():
-        # dataSet：[element, count]
-        localD = {}
-        for item in tranSet:
-            if item in freqItemSet: # 过滤，只取该样本中满足最小支持度的频繁项
-                localD[item] = headerTable[item][0] # element : count
-        if len(localD) > 0:
-            # 根据全局频数从大到小对单样本排序
-            orderedItem = [v[0] for v in sorted(localD.items(), key=lambda p:p[1], reverse=True)]
-            # 用过滤且排序后的样本更新树
-            updateFPtree(orderedItem, retTree, headerTable, count)
-    return retTree, headerTable
-'''
-
-def create_FPtree(frequent_item_table,head_table,MINSUP):
+def create_FPtree(frequent_item_table, head_table):
     root_node = TreeNode('Null', 1, None)
     for frequent_item in frequent_item_table:
         update_FPtree(frequent_item, root_node, head_table)
+    return root_node
+
+
+def ascend_FPtree(leaf_node, perfix_path):
+    if leaf_node.parent is not None:
+        perfix_path.append(leaf_node.key)
+        ascend_FPtree(leaf_node.parent, perfix_path)
+
+
+def find_prefix_path(head_table_item, head_table):
+    cond_patten_list = []
+    patten_item_frequency_set = {}
+    sub_head_table = {}
+    cur_node = head_table[head_table_item].node_link
+    while cur_node is not None:
+        prefix_path = []
+        ascend_FPtree(cur_node, prefix_path)
+        for item in prefix_path:
+            if item in patten_item_frequency_set:
+                patten_item_frequency_set[item] += 1
+            else:
+                patten_item_frequency_set[item] = 1
+        if len(prefix_path) > 0:
+            for num in range(cur_node.frequency):
+                cond_patten_list.append(prefix_path)
+        cur_node = cur_node.node_link
+        for k,v in patten_item_frequency_set.items():
+            if v > MINSUP:
+                new_dict_value = DictValue(v)
+                sub_head_table[k] = new_dict_value
+        cond_patten_list = sorted(cond_patten_list)
+        sub_head_table = dict(sorted(sub_head_table.items(), key=lambda x: x[1].frequency, reverse=True))
+    return cond_patten_list, sub_head_table
+
+def create_conditional_FPtree():
+    pass
+
 
 if __name__ == "__main__":
+
     data = csv_reader()
+
+    candidate_patten_set = []
+
     head_table = compute_item_frequency(data)
-    head_table = dict(sorted(head_table.items(), key=lambda x: x[1].frequency, reverse=True))
+
     frequent_item_table = compute_frequent_item(data,head_table)
 
+    root_node = create_FPtree(frequent_item_table, head_table)
+
+    # root_node.display()
+
+    for head_table_item in reversed(list(head_table)):
+        cond_patten_list, sub_head_table = find_prefix_path(head_table_item, head_table)
+        for item in sub_head_table:
+            if len(item) == 2:
+                candidate_patten_set.append(item)
+            else:
+                #big_recrusion(sub_head_table, cond_patten_list, candidate_patten_set)
+                pass
 
 
+'''
+frequent_item_table: [
+    ['soda', 'waffles']
+    ['chocolate', 'root vegetables', 'sausage']
+    ['UHT-milk', 'frozen vegetables', 'long life bakery product', 'root vegetables', 'whipped/sour cream', 'yogurt']
+    ['hamburger meat', 'onions', 'rolls/buns', 'whole milk']
+    ['bottled water', 'domestic eggs', 'fruit/vegetable juice', 'margarine', 'rolls/buns', 'tropical fruit', 'whipped/sour cream', 'whole milk']
+]
+
+cond_patten_list: [
+    ['onions', 'cream cheese ', 'UHT-milk']
+    ['onions', 'cream cheese ', 'bottled water']
+    ['onions', 'curd']
+    ['onions', 'curd']
+    ['onions', 'curd']
+    ['onions', 'curd', 'bottled water']
+    ['onions', 'curd', 'citrus fruit', 'bottle']
+]
+
+head_table: {
+    'whole milk': <__main__.DictValue object at 0x10c183198>, 
+    'other vegetables': <__main__.DictValue object at 0x10c183278>, 
+    'rolls/buns': <__main__.DictValue object at 0x10c1833c8>
+}
+
+sub_head_table: {
+    'tropical fruit': <__main__.DictValue object at 0x10a01c048>,
+    'other vegetables': <__main__.DictValue object at 0x109fe7f98>
+}
+'''
